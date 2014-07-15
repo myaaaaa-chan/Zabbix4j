574d5fcc2655c341ea5e108ef1af04980682ad1f
Suguru Yajima <myaaaaa.chan@gmail.com>
2014-07-15 20:36:50 +0900

    change getter/setter method to Lombok @Data annotation

--- src/main/java/com/zabbix4j/GetRequestCommonParams.java
+++ src/main/java/com/zabbix4j/GetRequestCommonParams.java
@@ -25,12 +25,14 @@
 package com.zabbix4j;
 
 import com.zabbix4j.utils.ZbxListUtils;
+import lombok.Data;
 
 import java.util.List;
 
 /**
- * Created by Suguru Yajima on 2014/05/02.
+ * @author uguru Yajima on 2014/05/02.
  */
+@Data
 public class GetRequestCommonParams {
     private Boolean countOutput;
     private Boolean editable;
@@ -52,177 +54,8 @@ public class GetRequestCommonParams {
     public GetRequestCommonParams() {
     }
 
-    public String getSelectConditions() {
-        return selectConditions;
-    }
-
-    public void setSelectConditions(String selectConditions) {
-        this.selectConditions = selectConditions;
-    }
-
-    public String getSelectOperations() {
-        return selectOperations;
-    }
-
-    public void setSelectOperations(String selectOperations) {
-        this.selectOperations = selectOperations;
-    }
-
-    public Boolean getCountOutput() {
-        return countOutput;
-    }
-
-    public void setCountOutput(Boolean countOutput) {
-        this.countOutput = countOutput;
-    }
-
-    public Boolean getEditable() {
-        return editable;
-    }
-
-    public void setEditable(Boolean editable) {
-        this.editable = editable;
-    }
-
-    public Boolean getExcludeSearch() {
-        return excludeSearch;
-    }
-
-    public void setExcludeSearch(Boolean excludeSearch) {
-        this.excludeSearch = excludeSearch;
-    }
-
-    public void setLimit(Integer limit) {
-        this.limit = limit;
-    }
-
-    public Boolean getPreservekeys() {
-        return preservekeys;
-    }
-
-    public void setPreservekeys(Boolean preservekeys) {
-        this.preservekeys = preservekeys;
-    }
-
-    public Boolean getSearchByAny() {
-        return searchByAny;
-    }
-
-    public void setSearchByAny(Boolean searchByAny) {
-        this.searchByAny = searchByAny;
-    }
-
-    public Boolean getSearchWildcardsEnabled() {
-        return searchWildcardsEnabled;
-    }
-
-    public void setSearchWildcardsEnabled(Boolean searchWildcardsEnabled) {
-        this.searchWildcardsEnabled = searchWildcardsEnabled;
-    }
-
-    public Integer getLimitSelects() {
-        return limitSelects;
-    }
-
-    public void setLimitSelects(Integer limitSelects) {
-        this.limitSelects = limitSelects;
-    }
-
-    public void addSortField(String field) {
-        sortfield = ZbxListUtils.add(sortfield, field);
-    }
-
-    public void setNodeId(int id) {
-        nodeids = ZbxListUtils.add(nodeids, id);
-    }
-
-    public void setSortField(String field) {
-        sortfield = ZbxListUtils.add(sortfield, field);
-    }
-
-    public Boolean isCountOutput() {
-        return countOutput;
-    }
-
-    public Boolean isEditable() {
-        return editable;
-    }
-
-    public Boolean isExcludeSearch() {
-        return excludeSearch;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
-
-    public void setLimit(int limit) {
-        this.limit = limit;
-    }
-
-    public List<Integer> getNodeids() {
-        return nodeids;
-    }
-
-    public void setNodeids(List<Integer> nodeids) {
-        this.nodeids = nodeids;
-    }
-
-    public String getOutput() {
-        return output;
-    }
-
-    public void setOutput(String output) {
-        this.output = output;
-    }
-
-    public Boolean isPreservekeys() {
-        return preservekeys;
-    }
-
-    public String getSearch() {
-        return search;
-    }
-
-    public void setSearch(String search) {
-        this.search = search;
-    }
-
-    public Boolean isSearchByAny() {
-        return searchByAny;
-    }
-
-    public Boolean isSearchWildcardsEnabled() {
-        return searchWildcardsEnabled;
-    }
-
-    public List<String> getSortfield() {
-        return sortfield;
-    }
-
-    public void setSortfield(List<String> sortfield) {
-        this.sortfield = sortfield;
-    }
-
-    public List<SortOrder> getSortorder() {
-        return sortorder;
-    }
-
-    public void setSortorder(List<SortOrder> sortorder) {
-        this.sortorder = sortorder;
-    }
-
-    public String getStartSearch() {
-        return startSearch;
-    }
-
-    public void setStartSearch(String startSearch) {
-        this.startSearch = startSearch;
-    }
-
     public void setSortOrder(SortOrder order) {
         sortorder = ZbxListUtils.add(sortorder, order);
-
     }
 
     public class SortOrder {
